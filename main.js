56c22ff568c2f421e0e11a1960b34d067ffcfa71
tmilne1 <tmilne1@gmail.com>
2019-12-07 23:25:54 -0500

    added event listeners, addedd function to modify children of the ul (.suggestions)

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
 
-
+window.onload = function() {
     const cities = []
     fetch(endpoint)                // fetch data from url - get promise
         .then((prom) => prom.json() ) //use the json methond of the promise to get another promise which can be turned into a json object  
@@ -12,7 +15,30 @@ function findMatch(match, cities){
          return place.city.match(regex) || place.state.match(regex)
     })
 }
+const input = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions')
 
+function search() {
+    const matches = findMatch(input.value, cities); // array of all the matches based on the input
+    
+    const child = matches.map((place)=>{
+        const regex = new RegExp(this.value, 'gi')
 
+        const city = place.city.replace(regex, `<span class=hl>${this.value}</span>`)
+        const state = place.state.replace(regex, `<span clsaa=hl> ${this.value}</span>`)
+        const population = numberWithCommas(place.population)
 
- 
+        return (`
+        <li>
+        <span>${city}, ${state}</span> 
+        <span>${population}</span>
+        </li>
+    `)
+    }).join(' ');
+    suggestions.innerHTML = child
+
+}
+
+input.addEventListener('keyup', search )
+
+}
