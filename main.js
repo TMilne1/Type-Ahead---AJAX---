dcf6300a4620de45db876262f98decc0baf27b80
tmilne1 <tmilne1@gmail.com>
2019-12-07 22:15:44 -0500

    added function to filter out desired city based on a given input

--- main.js
+++ main.js
@@ -3,7 +3,16 @@ const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb
 
     const cities = []
     fetch(endpoint)                // fetch data from url - get promise
-        .then((prom) => prom.json() ) //use the json methond of the promise to get another promise which can be turned to json   
+        .then((prom) => prom.json() ) //use the json methond of the promise to get another promise which can be turned into a json object  
        .then((jsonObjs) =>  {cities.push(...jsonObjs)}  )           //use the spread operate to split the arguments into array
 
+function findMatch(match, cities){
+    const regex = new RegExp(match,'gi');
+     return cities.filter((place)=>{
+         return place.city.match(regex) || place.state.match(regex)
+    })
+}
 
+
+
+ 
