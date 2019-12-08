3696b0aa13891112aa835ca462fcb54f43c8f68c
tmilne1 <tmilne1@gmail.com>
2019-12-07 21:27:53 -0500

    fetched city data from website

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
-console.log('hello')
-window.onload = function(){
 
 
+    const cities = []
+    fetch(endpoint)                // fetch data from url - get promise
+        .then((prom) => prom.json() ) //use the json methond of the promise to get another promise which can be turned to json   
+       .then((jsonObjs) =>  {cities.push(...jsonObjs)}  )           //use the spread operate to split the arguments into array
 
 
-
-}
